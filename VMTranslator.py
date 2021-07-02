2728bc56c8bf726489a2c6a823212100e9f36145
quochung2k2 <hungnguyenquoc8a2@gmail.com>
2021-07-02 11:18:53 +0930

    Create VMTranslator to translate VM language into Hack Assembly

---
+++ VMTranslator.py
@@ -0,0 +1,12 @@
+import sys
+
+in_file = open(sys.argv[1], "r")
+out_file = open(sys.argv[1].replace(".vm", ".asm"), "w")
+
+code_data = in_file.read().split("\n")
+
+for line in code_data:
+    line = line.strip()
+    if len(line) > 0:
+        if line[0:2] != "//":
+            out_file.write(line + "\n")      
