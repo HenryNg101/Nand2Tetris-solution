49dc00f79043c1d0a0178727e535fd8ca0c262e8
quochung2k2 <hungnguyenquoc8a2@gmail.com>
2021-07-31 23:23:32 +0930

    Implement APIs for symbol table and VM writer

---
+++ SymbolTable.py
@@ -0,0 +1,23 @@
+#This module is used for creating and using symbol table`
+
+class Table:
+    def __init__(self):
+        pass
+
+    def startSubroutine(self):
+        pass
+
+    def define(self):
+        pass
+
+    def varcount(self):
+        pass
+
+    def kindof(self):
+        pass
+
+    def typeof(self):
+        pass
+
+    def indexof(self):
+        pass
---
+++ VMWriter.py
@@ -0,0 +1,32 @@
+#This module is used to emits VM commands, using the VM command syntax
+
+class Writer:
+    def __init__(self):
+        pass
+
+    def writePush(self):
+        pass
+
+    def writePop(self):
+        pass
+
+    def writeArithmetic(self):
+        pass
+
+    def writeLabel(self):
+        pass
+
+    def writeGoto(self):
+        pass
+
+    def writeIf(self):
+        pass
+
+    def writeCall(self):
+        pass
+
+    def writeFunction(self):
+        pass
+
+    def writeReturn(self):
+        pass
