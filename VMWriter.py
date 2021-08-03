e3a3fdd44fd3679e5d673bccd68aba67f28455af
quochung2k2 <hungnguyenquoc8a2@gmail.com>
2021-08-03 23:57:40 +0930

    a

--- VMWriter.py
+++ VMWriter.py
@@ -1,7 +1,16 @@
 #This module is used to emits VM commands, using the VM command syntax
 
 class Writer:
-    def __init__(self):
+    def __init__(self, file_name):
+        self.class_name = file_name
+
+    """The main function to write VM, this function call other function"""
+    def generate_code(self, top):
+        #First, take the class name (For function name, etc)
+        #Then, process class variable declaration (until there's no variable to process)
+        #Then, process methods:
+        #- For each methods, process method parameters (if there's any)
+        #- Then, process expressions. For each expression, check if there's any expression inside expression, if yes, call appropriate function to process that expression, and so on.
         pass
 
     def writePush(self):
